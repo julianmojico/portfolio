50e2f2018a081642bad525c5540476422c787899
Julian Mojico <jmojico@gmail.com>
2022-07-04 20:55:02 -0300

    Updates

--- siteConfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "Julian Mojico",
-  shortName: "Portfolio",
-  description: "Design, gaming and development",
-  url: "https://julianmojico.com", // Domain of your site without prefix!
-  prefix: "/",
-  author: "Julian Mojico",
-  twitter: "https://twitter.com/Cepii_Mojico",
-}
